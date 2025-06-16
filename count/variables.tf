c96c325868dc1ef1469089a7ea50738a8fbdfe74
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-16 23:46:25 +0530

    created

---
+++ count/variables.tf
@@ -0,0 +1,12 @@
+variable "instances" {
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+  default = "Z05476892IBG9W47WAZ1Z"
+}
+
+variable "domain_name" {
+  default = "vrushaba.shop"
+
+}
