f57a51287b2ef2480fda4e938dd67f0ece23a80c
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-16 20:05:04 +0530

    crearted a file

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,11 +1,8 @@
 resource "aws_instance" "this" {
-  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  ami                    = var.ami_id # This is our devops-practice AMI ID
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  instance_type          = "t3.micro"
-  tags = {
-    Name    = "terraform-demo"
-    Purpose = "terraform-practice"
-  }
+  instance_type          = var.instance_type
+  tags                   = var.ec2_tags
 }
 
 resource "aws_security_group" "allow_tls" {
@@ -13,10 +10,10 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic and all outbound traffic"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = var.from_port
+    to_port     = var.to_port
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.cidr_blocks
   }
 
   egress {
@@ -26,7 +23,6 @@ resource "aws_security_group" "allow_tls" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "allow_tls"
-  }
+  tags = var.sg_tags
+
 }
