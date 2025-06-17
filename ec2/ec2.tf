0d9e74fec8dc8924a3560db3490a0d2faaa469da
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-17 11:36:03 +0530

    modified

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = "t3.micro"
   tags = {
-    Name    = "terraform-demo"
-    Purpose = "terraform-practice"
+    Name    = "terraform-KODURU"
+    Purpose = "terraform-KODURU"
   }
 }
 
