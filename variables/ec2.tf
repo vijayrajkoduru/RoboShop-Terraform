74c0a69b4b6427121bdc94fe2316da835527e617
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-17 19:59:58 +0530

    created

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,31 +1,31 @@
 resource "aws_instance" "this" {
-    ami = var.ami_id
-    vpc_security_group_ids  =   [aws_security_group.full_control.id]
-    instance_type   =   var.instance_type
-    tags    =   var.ec2_tags
-   }
+  ami                    = var.ami_id
+  vpc_security_group_ids = [aws_security_group.full_control.id]
+  instance_type          = var.instance_type
+  tags                   = var.ec2_tags
+}
 
 resource "aws_security_group" "full_control" {
-    name        = "full_control"
-    description = "allowing inbound and outbound"
+  name        = "full_control"
+  description = "allowing inbound and outbound"
 
-ingress{
+  ingress {
     from_port   = var.from_port
     to_port     = var.to_port
     protocol    = "tcp"
     cidr_blocks = var.cidr_blocks
-}
-egress{
-    from_port   =   0
-    to_port     =   0
-    protocol    =   "-1"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = var.cidr_blocks
 
-}
+  }
 
-tags={
-    Name= "newone"
-}
+  tags = {
+    Name = "newone"
+  }
 
-    }
+}
 
--- variables/variables.tf
+++ variables/variables.tf
@@ -1,28 +1,28 @@
 variable "ami_id" {
-  default     ="ami-09c813fb71547fc4f" 
+  default = "ami-09c813fb71547fc4f"
 }
 variable "instance_type" {
   default = "t3.micro"
 }
 variable "ec2_tags" {
-  type = map
+  type = map(any)
   default = {
-    Project = "koduru"
-    component = "backend"
+    Project     = "koduru"
+    component   = "backend"
     Environment = "dev"
-    Name  = "KODURU"
+    Name        = "KODURU"
   }
 }
 variable "cidr_blocks" {
   default = ["0.0.0.0/0"]
 }
 variable "to_port" {
-    type = number
-    default = 0
+  type    = number
+  default = 0
 }
 
 variable "from_port" {
-    type =  number
-    default = 0
-  
+  type    = number
+  default = 0
+
 }
