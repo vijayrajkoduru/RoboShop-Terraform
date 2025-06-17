91197987703296e6b08941908c25088d8e101d89
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-17 19:58:37 +0530

    created

--- variables/variables.tf
+++ variables/variables.tf
@@ -1,11 +1,9 @@
 variable "ami_id" {
-  default     ="ami-09c813fb71547fc4f"
-  
+  default     ="ami-09c813fb71547fc4f" 
 }
 variable "instance_type" {
   default = "t3.micro"
 }
-
 variable "ec2_tags" {
   type = map
   default = {
@@ -15,11 +13,9 @@ variable "ec2_tags" {
     Name  = "KODURU"
   }
 }
-
 variable "cidr_blocks" {
   default = ["0.0.0.0/0"]
 }
-
 variable "to_port" {
     type = number
     default = 0
