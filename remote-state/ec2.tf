e77cf686472724e5548755a70af3311bcd5e53d5
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-19 03:58:08 +0530

    again checking

---
+++ remote-state/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "state" {
+  ami                    = var.ami_id
+  vpc_security_group_ids = [aws_security_group.delete.id]
+  instance_type          = var.instance_id
+
+  tags = var.ec2_tgs
+}
+resource "aws_security_group" "delete" {
+  name        = "delete"
+  description = "inbound and outbound"
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = var.protocol
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = var.sg_n
+  }
+}
