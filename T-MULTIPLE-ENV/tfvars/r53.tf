6889461857b86a64f46e3055a459328f5f7d5dd8
vijayrajkoduru <vijayrajkoduruai@gmail.com>
2025-06-19 23:47:02 +0530

    created:

--- T-MULTIPLE-ENV/tfvars/r53.tf
+++ T-MULTIPLE-ENV/tfvars/r53.tf
@@ -1,14 +1,14 @@
-/* resource "aws_route53_record" "Hacker" {
+ resource "aws_route53_record" "Hacker" {
   count           = length(var.instances)
   zone_id         = var.zone_id
-  name            = "${var.instances[count.index]}.${var.domain_name}"
+  name            = var.instances[count.index] == "frontend" && var.environment == "prod" ? var.domain_name : "${var.instances[count.index]}-${var.environment}.${var.domain_name}"
   type            = "A"
   ttl             = 1
-  records         = [aws_instance.Hacker[count.index].private_ip]
+  records         = [var.instances[count.index] == "frontend" && var.environment == "prod" ? aws_instance.Hacker[count.index].public_ip : aws_instance.Hacker[count.index].private_ip]
   allow_overwrite = true
 }
-
-resource "aws_route53_record" "frontend" {
+ 
+/* resource "aws_route53_record" "frontend" {
   zone_id         = var.zone_id
   name            = var.domain_name
   type            = "A"
